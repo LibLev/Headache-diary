45b6cdf5f95eb6703b6bb8691ea5d8a90e54cce8
LibLev <libardi.levente@gmail.com>
2019-05-07 11:55:40 +0200

    registration has been finished

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
 from flask import Flask, render_template, redirect, session, request
 from database import queries
+import hash
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+
     return render_template('index.html')
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    if request.method == 'POST':
+        username, first_name, last_name, password, email = get_registration_data()
+        check_name = queries.find_user(u_name=username)
+        if check_name['user_name'] is not None:
+            return render_template('registration.html',
+                                   message='Sorry, This user name is already in use. Please Select another')
+        queries.insert_new_user(u_name=username, f_name=first_name, l_name=last_name, pword=password, email=email)
+        return redirect('/')
+    return render_template('registration.html')
+
+
+def get_registration_data():
+    user_name = request.form.get('username')
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    e_mail_address = request.form.get('email_address')
+    password = hash.hash_password(request.form.get('password'))
+    return user_name, first_name, last_name ,password, e_mail_address
 
 
 if __name__ == '__main__':
--- database/queries.py
+++ database/queries.py
@@ -6,3 +6,19 @@ def test():
     SELECT id FROM users
     ''')
 
+
+def find_user(u_name):
+    return data_manager.execute_select('''
+    select user_name, id
+    from users
+    where user_name=%(username)s''',
+                                       variables={'user_name': u_name})
+
+
+def insert_new_user(u_name, pword, email, f_name, l_name):
+    return data_manager.execute_select("""
+    insert into users (user_name,first_name,last_name,hashed_password,email_address)
+    values (%(username)s, %(first_name)s, %(last_name)s, %(h_password)s, %(e_mail)s)
+    """,
+                                       variables={'user_name': u_name, 'first_name': f_name, 'last_name': l_name,
+                                                  'hashed_password': pword, 'email_address': email})
